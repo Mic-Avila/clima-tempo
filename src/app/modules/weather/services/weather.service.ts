87ab84846a4dabac088e22f424aa68345af41da6
michael.avila <michael.avila@avancoinfo.com.br>
2023-07-27 10:35:23 -0300

    ajuste gitgnore e api

--- src/app/modules/weather/services/weather.service.ts
+++ src/app/modules/weather/services/weather.service.ts
@@ -1,18 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/env/enviroment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  private APIKEY = '97516a5dea5404b2f8f80598945cbdc3'
-
   constructor(private http: HttpClient) { }
 
 
   buscaDadosAPI(cidade: string):Observable<any>{
-    return this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&mode=json&appid=${this.APIKEY}`,{})
+    return this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&mode=json&appid=${environment.api}`,{})
   }
 }
